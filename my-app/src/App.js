94f4a0fa69131fb94161704cd15e79f903ea6526
YJ <simyj547@gmail.com>
2022-01-23 22:20:40 +0900

    20220123 react practice commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,14 @@
 import './App.css';
-import Event from './components/2-7Event/Event';
+import ThankYouDialog from './components/3-6 Composition2/ThankYouDialog';
+//import Dialog from './components/3-6 Composition2/Dialog';
+//import WelcomeDialog from './components/3-5Composition/WelcomeDialog';
+//import Reducer from './components/3-3 Hooks/Reducer';
+//import State from './components/3-3 Hooks/State';
+//import ControlledComponet from './components/2-10 Form/ControlledComponet';
+//import UncontrolledComponent from './components/2-10 Form/UncontrolledComponent';
+//import List from './components/2-9List/List';
+//import Condition from './components/2-8ConditionalRendering/Condition';
+//import Event from './components/2-7Event/Event';
 //import ClassComponent2 from './components/2-6 LifeCycle/ClassComponent';
 //import FunctionalComponent from './components/2-5 State/FunctionalComponent';
 //import ClassComponent from './components/2-5 State/ClassComponent';
@@ -9,7 +18,15 @@ import Event from './components/2-7Event/Event';
 function App() {
   return (
     <div className="App">
-      <Event/>
+      <ThankYouDialog/>
+      {/* <Dialog/> */}
+      {/* <Reducer/>
+      <State/> */}
+      {/* <UncontrolledComponent/>
+      <ControlledComponet/> */}
+      {/* <List/> */}
+      {/* <Condition/> */}
+      {/* <Event/> */}
       {/* <ClassComponent2/> */}
       {/* <FunctionalComponent/>
       <ClassComponent/>
---
+++ my-app/src/components/2-8ConditionalRendering/Condition.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function UserGreeting(props){
+    return <h1>{props.name && `${props.name},`} Welcome { props.count ?  `It's ${props.count} times` : null}</h1>; 
+}
+
+
+function GuestGreeting(props){
+    return <h1>Please sign up.</h1>
+}
+
+function Greeting(props){
+    // if(props.isLoggedIn){
+    //     return <UserGreeting name="Inkor" count={0} />
+    // }
+    // return <GuestGreeting />;
+    return props.isLoggedIn ? <UserGreeting name="inkor" count={0} /> : <GuestGreeting />;
+}
+
+export default function Condition() {
+    const isLoggedIn = true;
+  return (
+      <div>
+          <Greeting isLoggedIn={isLoggedIn}/>
+      </div>
+    )
+}
