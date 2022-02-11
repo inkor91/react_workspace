ad9a399eb365563f6a1ff8073370285d39286610
YJ <simyj547@gmail.com>
2022-02-11 21:21:51 +0900

    react learn-middleware commit

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,13 @@
+import CounterContainer from "./components/CounterContainer";
+//import SampleContainer from "./containers/SampleContainer";
+
+const App = () => {
+  return (
+    <div>
+      <CounterContainer/> 
+      {/* <SampleContainer/> */}
+    </div>
+  );
+};
+
+export default App;
