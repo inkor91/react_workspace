7bd037f42ac59cd5599b6e8d641f19050fe1420a
YJ <simyj547@gmail.com>
2022-02-21 17:17:23 +0900

    webpack setting and build commit

---
+++ gugudan/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval', // hidden-source-map
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  entry: {
+    app: './client',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: ['> 5% in KR', 'last 2 chrome versions'], // browserslist 사이트 참고
+                },
+                debug: true,
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: [],
+        },
+      },
+    ],
+  },
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+  },
+};
