708e5e8ff03453e9a801fbd79372d9308e66849f
YJ <simyj547@gmail.com>
2022-03-04 23:31:36 +0900

    Numberbaseball add props commit

--- numberbaseball/NumberBaseball.jsx
+++ numberbaseball/NumberBaseball.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import Try from './Try';
 
 function getNumbers(){ // 숫자 네 개를 겹치지 않고 랜뎀하게 뽑는 함수
-
+    const candidate = [1,2,3,4,5,6,7,8,9];
+    const array = [];
+    for(let i =0;i<4;i+=1){
+        const chosen = candidate.splice(Math.floor(Math.random()*(9 - i)), 1)[0];
+        array.push(chosen);
+    }
+    return array;
 }
 
 class NumberBaseball extends Component {
@@ -12,13 +19,54 @@ class NumberBaseball extends Component {
         tries: [],
     };
 
-    onSubmitForm = () =>{
-
+    onSubmitForm = (e) =>{
+        e.preventDefault();
+        if(this.state.value === this.state.answer.join('')){
+            this.setState({
+                result:'홈런!',
+                tries:[...this.state.tries, {try:this.state.value, result:'홈런!'}],
+            })
+            alert('게임을 다시 시작합니다!');
+            this.setState({
+                value:'',
+                answer:getNumbers(),
+                tries:[],
+            });
+        }else{
+            const answerArray = this.state.value.split('').map((v)=>parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if(this.state.tries.length>=9){
+                this.setState({
+                    result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')}였습니다!`,
+                });
+                alert('게임을 다시 시작합니다!');
+                this.setState({
+                    value:'',
+                    answer:getNumbers(),
+                    tries: [],
+                });
+            }else{
+                for(let i=0; i<4; i+=1){
+                    if(answerArray[i]===this.state.answer[i]){
+                        strike +=1;
+                    } else if (this.state.answer.includes(answerArray[i])){
+                        ball +=1;
+                    }
+                }
+                this.setState({
+                    tries: [...this.state.tries, {try:this.state.value, result:`${strike} 스트라이크, ${ball} 볼입니다`}],
+                    value: '',
+                });
+            }
+        }
     };
-    onChangeInput = () =>{
-
-    };
-
+    onChangeInput = (e) =>{
+        console.log(this.state.answer);
+        this.setState({
+            value: e.target.value,
+        });
+    };  
 
     render(){
         return(
@@ -29,63 +77,11 @@ class NumberBaseball extends Component {
                 </form>
                 <div>시도: {this.state.tries.length}</div>
                 <ul>
-                    {['사과','바나나','포도','귤','배', '밤'].map((v)=>{
+                    {this.state.tries.map(  (v,i) => {
                         return(
-                        <li>{v}</li>
-                        );
-                    })}
-
-                    {[
-                        ['사과','맛있다'],
-                        ['바나나','맛있다'],
-                        ['포도','시다'],
-                        ['귤','맛있다'],
-                        ['배','크다'],
-                        ['밤','고소하다']
-                        ].map((v)=>{
-                            return(
-                            <li><b>{v[0]}</b> - {v[1]}</li>
-                        );
-                    })}
-                    
-                    {[
-                        { fruit: '사과', taste: '사과맛'},
-                        { fruit:'바나나', taste:'맛있다'},
-                        { fruit:'포도',taste:'시다'},
-                        { fruit:'귤',taste:'맛있다'},
-                        { fruit:'배',taste:'크다'},
-                        { fruit:'밤',taste:'고소하다'},
-                        ].map((v)=>{
-                            return(
-                            <li key={v.fruit + v.taste}><b>{v.fruit}</b> - {v.taste}</li>
-                        );
-                    })}
-                    
-                    {/* return x */}
-                    {[
-                        { fruit: '사과', taste: '사과맛'},
-                        { fruit:'바나나', taste:'맛있다'},
-                        { fruit:'포도',taste:'시다'},
-                        { fruit:'귤',taste:'맛있다'},
-                        { fruit:'배',taste:'크다'},
-                        { fruit:'밤',taste:'고소하다'},
-                        ].map((v)=>
-                            <li key={v.fruit + v.taste}><b>{v.fruit}</b> - {v.taste}</li>
-                    )}
-
-                       {/* i 추가 x */}
-
-                       {[
-                        { fruit: '사과', taste: '사과맛'},
-                        { fruit:'바나나', taste:'맛있다'},
-                        { fruit:'포도',taste:'시다'},
-                        { fruit:'귤',taste:'맛있다'},
-                        { fruit:'배',taste:'크다'},
-                        { fruit:'밤',taste:'고소하다'},
-                        ].map((v,i)=>
-                            <li key={v.fruit + v.taste}><b>{v.fruit}</b> - {i}</li>
-                    )}
-
+                            <Try key={`${i+1}차 시도:`} tryInfo={v}/>
+                            );
+                        })}
                 </ul>
             </>
         );
